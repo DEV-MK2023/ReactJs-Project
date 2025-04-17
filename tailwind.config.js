d966502d865653849e8f522f6a1340a7e8bf362f
Muneeb Zafar <165970268+DEV-MK2023@users.noreply.github.com>
2025-04-18 03:54:42 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: [
+    "./index.html",
+    "./src/portfolio/navbar.jsx",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+  ],
+}
